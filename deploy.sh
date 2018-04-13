4b4e038136238b26ab18ba06cdb60f33e4bfb4bc
Nicola Strappazzon C <nicola@nicola.local>
2018-04-13 17:12:16 +0200

    Add branch arg for deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,40 @@
 # Set default values:
 BRANCH=master
 
+# Help message of usage this script.
+usage()
+{
+  cat << EOF
+Script to deploy puppet in MasterLess mode. You can deploy specific branch and
+run in test mode.
+
+usage: $0 [--branch=name]
+
+$0 --branch=IN-1234
+
+OPTIONS:
+  --branch Specify name of branch in git repository to deploy
+
+EOF
+}
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --branch=*)
+      BRANCH="${1#*=}"
+      ;;
+    --help)
+      usage
+      exit 0
+      ;;
+    *)
+      printf "Error: Invalid argument.\n"
+      usage
+      exit 1
+  esac
+  shift
+done
+
 # Make sure only root can run our script
 if [[ $EUID -ne 0 ]]; then
   echo "This script must be run as root"
